5fa2ca8d9fba9c8d078a15b9f4699641542d41e3
mildrette <mildredfonka1@gmail.com>
2024-10-11 15:30:05 +0100

    double in java

---
+++ variables/marketShare.java
@@ -0,0 +1,21 @@
+
+import java.awt.SystemTray;
+
+public class countComments {
+    public static void main(String[] args) {
+        System.out.println(" Mildred is learning Java, and Has 3 months to learn and get over the basic");
+    }
+}
+
+
+// Doubles
+
+public class marketShare {
+    public static void main(String[] args) {
+        double androidShare = 95.5;
+        System.out.println(androidShare);
+
+        double androidMarketShare = 10000780;
+        System.out.println(androidMarketShare);
+    }
+}
