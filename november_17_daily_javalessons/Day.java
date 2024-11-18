ef7394011732e3ca63340eba4dde34a23abe4515
mildrette <mildredfonka1@gmail.com>
2024-11-18 01:34:08 +0100

    learn how to print the current date that is the date of today on the terminal in java

--- november_17_daily_javalessons/Day.java
+++
@@ -1,5 +0,0 @@
-public class Day{
-    public static void main(String[] args) {
-        System.out.println("Hello there, I am learning Java");
-    }
-}
---
+++ november_17_daily_javalessons/Main.java
@@ -0,0 +1,33 @@
+// public class Main{
+//     public static void main(String[] args) {
+// // data type variables
+// int age =  25;
+// int myAge = age;
+// System.out.println(myAge);
+//     }
+// }
+
+// types
+
+// 1. primitive Types
+
+//public class Main {
+//    public static void main(String[] args) {
+//        byte age = 30;
+//        boolean isReady = false;
+//        float price = 10.90F;
+//        long viewCount = 31234567890L;
+//        System.out.println(isReady);
+//    }
+//}
+
+import java.util.Date;
+// this code will print out the date of today on the terminal
+
+public class Main {
+    public static void main(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
